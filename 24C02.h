cff939b0bf210b80984b98b7fd1b3c017bbfcdd6
nitinsai7 <nitinsaigrandhe@gmail.com>
2025-10-05 20:04:39 +0530

    Add files via upload

---
+++ 24C02.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   24C02.h
+ * Author: PERSONAL
+ *
+ * Created on November 15, 2020, 12:24 PM
+ */
+
+#ifndef 24C02_H
+#define	24C02_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* 24C02_H */
+
